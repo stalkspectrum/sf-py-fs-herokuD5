2faa43538d4f6129caa2c2492ed5a80fe168b72b
Stalk Spectrum <stalkspectrum@gmail.com>
2020-02-27 14:38:16 +0800

    ALLOWED_HOSTS defined in settings.py

--- D4test/settings.py
+++ D4test/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +26,10 @@ SECRET_KEY = 'd$r-!veoxd2houpf9t@9$63dip*ep1$fk7d3f_f1apbac93uxp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'd5python.herokuapp.com',
+]
 
 
 # Application definition
